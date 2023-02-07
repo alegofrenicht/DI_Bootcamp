8a79fbe80ae7e99de03bcc045c52e7255d793ecc
alegofrenicht <lalala@poopsex.com>
2023-02-08 02:24:00 +0300

    week6 day4 dailychallenge

---
+++ Week6/Day4/DailyChallenge/dailychallenge.py
@@ -0,0 +1,40 @@
+# Instructions
+# Challenge 1
+# Ask the user for a number and a length.
+# Create a program that prints a list of multiples of the number until the list length reaches length.
+# Examples
+#
+# number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
+#
+# number: 12 - length 10 ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+#
+# number: 17 - length 6 ➞ [17, 34, 51, 68, 85, 102]
+
+number = int(input('choose number:\n'))
+length = int(input('choose length:\n'))
+numbers = []
+for num in range(1, length + 1):
+    numbers.append(number * num)
+print(numbers)
+
+
+
+# Challenge 2
+# Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+# Examples
+#
+# user's word : "ppoeemm" ➞ "poem"
+#
+# user's word : "wiiiinnnnd" ➞ "wind"
+#
+# user's word : "ttiiitllleeee" ➞ "title"
+#
+# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+# Notes
+# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).
+
+string = input('enter your word:\n')
+string = ''.join(sorted(set(string), key=string.index))
+print(string)
+
+

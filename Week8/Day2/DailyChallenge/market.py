bc7ed01c65a59df63ab30325e333c4312d5b9377
alegofrenicht <lalala@poopsex.com>
2023-02-20 21:08:22 +0300

    editing

--- Week8/Day2/DailyChallenge/market.py
+++ Week8/Day2/DailyChallenge/market.py
@@ -16,6 +16,16 @@ class Farm:
         [print(f"{key} : {value}") for key, value in self.animals.items()]
         return '\n    E-I-E-I-0!'
 
+    def get_animal_types(self):
+        animal_types = []
+        [animal_types.append(type) for type in sorted(self.animals)]
+        return animal_types
+
+    def get_short_info(self):
+        animals = self.get_animal_types()
+        return f"McDonald’s farm has {animals[0]}s, {animals[1]}s and {animals[2]}."
+
+
 
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow', 5)

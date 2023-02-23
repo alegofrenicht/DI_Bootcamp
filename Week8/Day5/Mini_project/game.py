e2655793a56724e307287d796483ede8963fa56e
alegofrenicht <lalala@poopsex.com>
2023-02-23 21:34:20 +0300

    commit week8 day5 mini_project

---
+++ Week8/Day5/Mini_project/game.py
@@ -0,0 +1,63 @@
+from random import choice
+
+class Game:
+    def __init__(self):
+        self.items = ['rock', 'paper', 'scissors']
+        self.__user = ''
+        self.__computer = ''
+        self.__game_result = ''
+
+
+    def __get_user_item(self):
+        while True:
+            user_choice = input("Let's start the game, choose rock or paper or scissors! One, Two, Three and ...\n")
+            for item in self.items:
+                if item == user_choice:
+                    self.__user = user_choice
+                    return self.__user
+
+    def __get_computer_item(self):
+        self.__computer = choice(self.items)
+        return self.__computer
+
+    def __get_game_result(self, user_item, computer_item):
+        if user_item == computer_item:
+            self.__game_result = 'Draw'
+            return self.__game_result
+        else:
+            if user_item == 'rock':
+                if computer_item == 'scissors':
+                    self.__game_result = 'Win'
+                    return self.__game_result
+                else:
+                    self.__game_result = 'Loss'
+                    return self.__game_result
+            elif user_item == 'paper':
+                if computer_item == 'rock':
+                    self.__game_result = 'Win'
+                    return self.__game_result
+                else:
+                    self.__game_result = 'Loss'
+                    return self.__game_result
+            else:
+                if computer_item == 'paper':
+                    self.__game_result = 'Win'
+                    return self.__game_result
+                else:
+                    self.__game_result = 'Loss'
+                    return self.__game_result
+
+
+    def play(self):
+        self.__get_game_result(self.__get_user_item(), self.__get_computer_item())
+        print(f"You selected {self.__user}. The computer selected {self.__computer}. It's a {self.__game_result}\n")
+        return self.__game_result
+
+
+
+
+
+
+
+
+
---
+++ Week8/Day5/Mini_project/rock_paper_scissors.py
@@ -0,0 +1,32 @@
+from game import Game
+
+
+def get_user_menu_choice():
+    menu = input("\nMenu:\n(1) Play a new game\n(2) Show scores\n(0) Exit\n")
+    answers = ['1', '2', '0']
+    if menu not in answers:
+        get_user_menu_choice()
+    else:
+        return menu
+
+
+def print_results(results):
+    for result, number in results.items():
+        print(f"{result}: {number}")
+
+
+def main():
+    results = {}
+    while True:
+        a = get_user_menu_choice()
+        if a == '0':
+            break
+        elif a == '1':
+            game = Game()
+            game_result = game.play()
+            results[game_result] = results.get(game_result, 0) + 1
+        elif a == '2':
+            print_results(results)
+    print('Thank you for playing')
+
+main()

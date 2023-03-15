c2b22d98c6d1706a3ce12ccf1977a3e4378a2c4d
alegofrenicht <lalala@poopsex.com>
2023-03-16 00:50:17 +0300

    week11 day4 dailychallenge

---
+++ Week11/Day4/DailyChallenge/website.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    html = render_template('homepage.html')
+    return html
+
+
+@app.route('/<color>')
+def colors(color):
+    html = render_template('color.html', color=color)
+    return html
+
+
+app.run()
